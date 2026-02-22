c53ffc54ff4a40b8364f22d5e8cdb5687e9c8a45
srikalavalaboju <srikalavalaboju@gmail.com>
2026-02-22 20:57:19 +0530

    day1 progress

--- day1.py
+++ day1.py
@@ -1,1 +1,15 @@
-print("hello world")
+n=int(input("Enter the size:"))
+arr=[]
+for i in range(n):
+    val=int(input(f"Enter the elements{i+1}:"))
+    arr.append(val)
+    print(arr)
+found=False
+key=int(input("Enter the target element:"))
+for i in range(n):
+    if arr[i]==key:
+        found=True
+        print("Element found")
+        break
+if not found:
+    print("not found")
